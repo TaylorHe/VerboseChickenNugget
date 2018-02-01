2ff4fd4636b223560c683ebfa486c3cb64653e96
Taylor He <taylor.he7@gmail.com>
2018-02-01 17:09:06 -0500

    initial commit for assignment 2

---
+++ Assignment2/SubstringDivisibility.java
@@ -0,0 +1,14 @@
+/*
+ * Project Euler #43, with some modifications to support various lengths
+ * @author Taylor He
+ * @author Jacob Manzelmann
+ * I pledge my honor that I have abided by the Stevens Honor System.
+ *   - Taylor He, Jacob Manzelmann
+ */
+public class SubstringDivisibility {
+
+}
+
+public static void main(String[] args) {
+
+}
