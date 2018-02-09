c83daf2e836d7d7b5a2686a5f696831bfbfad346
jmanzelm <jmanzelm@stevens.edu>
2018-02-08 19:52:32 -0500

    Updated contacts.

--- Assignment3/contacts.c
+++ Assignment3/contacts.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define ALPHABET_LENGTH    26
 #define OPERATION_BUF_SIZE  5
 #define NAME_BUF_SIZE      22
 
+//node struct for trie
 typedef struct node {
     int num_children;
     struct node* children[ALPHABET_LENGTH];
 } trie_node;
 
+//function to malloc a new node with base parameters
 struct node* new_node() {
 	struct node* n_node = malloc(sizeof(int) + sizeof(struct node*) * 26);
 	n_node->num_children = 0;
@@ -18,6 +21,7 @@ struct node* new_node() {
 	return n_node;
 }
 
+//function to add a contact to the trie
 void add(struct node* trie, char* name) {
 	trie->num_children += 1;
 	if (*name != '\0') {
@@ -32,7 +36,8 @@ void add(struct node* trie, char* name) {
 	return;
 }
 
-int find(struct node* trie, char* partial) {
+//function that returns the number of contacts that start with "partial"
+int find(struct node* trie, char* partial){
 	if (trie == NULL) {
 		return 0;
 	}
@@ -42,8 +47,45 @@ int find(struct node* trie, char* partial) {
 	return trie->num_children;
 }
 
+//function to free the memory upon completion
+void free_trie(struct node* trie){
+  if (trie == NULL){
+    return;
+  }
+  for(int i = 0; i < ALPHABET_LENGTH; i++){
+    free_trie(trie->children[i]);
+  }
+  free(trie);
+}
+
+//main method asks for user input to add and find contacts
 int main() {
 	struct node* trie = new_node();
+  /*add(trie, "hello");
+  add(trie, "he");
+  printf("%d\n", find(trie, "h"));*/
+  int num_cases;
+  char name2[NAME_BUF_SIZE];
+  char* name = name2;
+  char buffer[OPERATION_BUF_SIZE+NAME_BUF_SIZE];
+  scanf("%d\n", &num_cases);
+  for(int i = 0; i < num_cases; i++){
+    fgets(buffer, OPERATION_BUF_SIZE+NAME_BUF_SIZE, stdin);
+    //printf("%s\n", buffer);
+    if(buffer[3] == ' '){
+      //memcpy(name, buffer+4, strlen(buffer+4)+1);
+      //printf("%s\n", name);
+      add(trie, buffer+4);
+    }
+    else{
+      //memcpy(name, buffer+5, strlen(buffer+5)+1);
+      //printf("%s\n", name);
+      printf("%d\n", find(trie, "h"));
+      printf("%d\n", find(trie, buffer+5));
+    }
+    memset(buffer, '\0', OPERATION_BUF_SIZE+NAME_BUF_SIZE);
+    memset(name, '\0', NAME_BUF_SIZE);
+  }
 
-	int num_cases = scanf()
+  free_trie(trie);
 }
