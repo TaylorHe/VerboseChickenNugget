f615967fd1cce2fcc9c1b34b8368ee7981dfef6e
Taylor He <taylor.he7@gmail.com>
2018-04-03 20:41:14 -0400

    running from Cloud Computing part 2

--- Assignment9/findmedian.py
+++ Assignment9/findmedian.py
@@ -9,6 +9,10 @@ from heapq import heappush, heappop, heapify
 """
 MinHeap is a wrapper around heapq
 Min heaps are supported by negating the input on the max heap
+Negation is due to heap[0] returning the smallest item
+Because this is the small number heap, we need the largest element.
+Negation will sort by the "largest" element, but we have to make
+sure to negate the pop() and peek() when returning value
 """
 class MinHeap:
     def __init__(self, h=[]):
@@ -34,7 +38,8 @@ class MinHeap:
 
 """
 MaxHeap is a wrapper around heapq
-Max heaps are inherently supported by heapq
+heap[0] returns the smallest element in the heap
+after heapify-ing, and that's exactly what we want
 """
 class MaxHeap:
     def __init__(self, h=[]):
@@ -65,15 +70,17 @@ def runningMedian(a):
     This is done by maintaining 2 heaps of same size
         max_heap:   all items > med is added to this heap
         min_heap:   all items < med is added to this heap
-    Also, absolute value of (len(max_heap) - len(min_heap)) >= 1
+    Also, diff of the size of both is <= 1 at all times
     The median is found by obtaining the top element in both heaps
     and finding the median of them
     """
     min_heap = MinHeap()
     max_heap = MaxHeap()
-    res = []
+    running_median_list = []
     median = 0
     for item in a:
+        # If the item is > median, push it on the max_heap
+        # else push to min_heap
         if item > median:
             max_heap.push(item)
         else:
@@ -86,8 +93,8 @@ def runningMedian(a):
         elif len(max_heap) > len(min_heap) + 1:
             min_heap.push(max_heap.pop())
 
-        # If heaps are same size, the median is the average   
-        # of max of min_heap and min of max_heap
+        # If heaps are same size, the median is the average of 
+        # the max of min_heap and the min of max_heap
         # Else, it's top of the larger of the 2
         if len(min_heap) == len(max_heap):
             median = (min_heap.peek() + max_heap.peek()) / 2.0
@@ -96,9 +103,10 @@ def runningMedian(a):
         else:
             median = float(max_heap.peek())
         
-        # Prints with 1 decimal point
-        res.append(round(median,1))
-    return res
+        # Appends to return list foratted with 1 decimal point
+        running_median_list.append(round(median,1))
+
+    return running_median_list
 
 if __name__ == '__main__':
     a = []
